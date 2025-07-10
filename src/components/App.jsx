6bb8145e59c536712f9651af353cce5cbfeefd10
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-07-10 14:38:23 +0300

    ok

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,11 @@
-import AppTasksAsyncReduxStore from "./home-work-7/AppTasksReduxAsync"
+
 
 
 function App() {
   return (
-    <>
-      <h1>React homework redone</h1>
-      <div
-        style={{
-          borderWidth: 3,
-          borderColor: 'brown',
-          borderStyle: 'solid',
-          padding: 15,
-        }}
-      >
-<AppTasksAsyncReduxStore/>
-      </div>
-    </>
+    <h1>
+New app
+    </h1>
   );
 }
 export default App;
