d8c08364ee9beacf387226b766d1f0f5396a4d23
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-07-26 22:02:40 +0300

    ok

--- src/helpers/categories.jsx
+++ src/helpers/categories.jsx
@@ -1,5 +1,5 @@
 
 
-export const mainExpenses = [{ type: "Groceries", color: "#ADFF2F" },{ type: "House care", color: "#8A2BE2" },{ type: "Self care", color: "#F08080" },{ type: "Child Care", color: "#6495ED" },{ type: "Utilities", color: "#B8860B" },{ type: "Education", color: "#008B8B" },{ type: "Leisure", color: "#BDB76B" },{ type: "Other types", color: "#FF8C00" }]
+export const mainExpenses = [{ type: "Groceries", color: "#ADFF2F" },{ type: "House care", color: "yellow" },{ type: "Self care", color: "#F08080" },{ type: "Child Care", color: "#6495ED" },{ type: "Utilities", color: "#B8860B" },{ type: "Education", color: "#008B8B" },{ type: "Leisure", color: "#BDB76B" },{ type: "Other types", color: "#FF8C00" }]
 export const mainIncomes = [{ type: "Salary", color: "#FF8C00" }, { type: "Project", color: "#FFF8DC" }, { type: "Gift", color: "#7FFF00" }, { type: "Donation", color: "#90EE90" }, { type: "Loan", color: "#FFB6C1" }, { type: "Other", color: "#ADD8E6" }]
 export const allCategories = mainExpenses.concat(mainIncomes)
