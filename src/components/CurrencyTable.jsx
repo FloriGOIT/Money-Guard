d1816d2484ce4acc88b25e00827f7f6faef539c5
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-08-01 06:45:16 +0300

    ok

--- src/components/CurrencyTable.jsx
+++ src/components/CurrencyTable.jsx
@@ -9,19 +9,19 @@ const CurrencyTable = () => {
   return (
     <div className={style.currencyTable}>  
       <div className={style.currancyTableHeader}>
-        <span></span>
-        <span>NBR</span>
-        <span>Buy</span>
-        <span>Sell</span>
-        <span></span>
+        <span className={style.currancyTableNotRate}></span>
+        <span className={style.currancyTableRate}>NBR</span>
+        <span className={style.currancyTableRate}>Buy</span>
+        <span className={style.currancyTableRate}>Sell</span>
+        <span className={style.currancyTableNotRate}></span>
       </div>
       {currency.map(currency => {
         const location = `/currency/${currency.currencyName}`;
         return (<div className={style.currancyTableRow} key={currency.currencyName}>
-          <span>{currency.currencyName}</span>
-          <span>{Number(currency.nbrRate).toFixed(4)}</span>
-          <span>{Number(currency.buyRate).toFixed(4)}</span>
-          <span>{Number(currency.sellRate).toFixed(4)}</span>
+          <span className={style.currancyTableNotRate}>{currency.currencyName}</span>
+          <span className={style.currancyTableRate}>{Number(currency.nbrRate).toFixed(4)}</span>
+          <span className={style.currancyTableRate}>{Number(currency.buyRate).toFixed(4)}</span>
+          <span className={style.currancyTableRate}>{Number(currency.sellRate).toFixed(4)}</span>
           <Link to={location} className={style.editCurrency}> <MdOutlineModeEdit /></Link>
         </div>)
       })}
--- src/pages/Currency.jsx
+++ src/pages/Currency.jsx
@@ -1,15 +1,24 @@
 import style from '../components/moneyGuard.module.scss';
 import Nav from '../components/Nav';
 import CurrencyTable from 'components/CurrencyTable';
-
+import { IoAddSharp } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
 const Currency = () => {
-  const returnLocation = "/currency";
+  const returnLocation = '/currency';
   return (
     <section className={style.currencyWrapper}>
       <Nav />
       <div className={style.currencyContainer}>
-        <h2>Currency exchange</h2>
+
+        <div className={style.currencyContainerHeader}>
+          <h2>Currency exchange</h2>
+          <Link to="/currency/newCoin" className={style.addCoinButton}>
+            <IoAddSharp />
+          </Link>
+        </div>
+
         <CurrencyTable returnLocation={returnLocation} />
+        
       </div>
     </section>
   );
