d326e8aa99637ce1987af2b23a50f0d5d3fbc677
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-08-15 15:41:52 +0300

    ok

--- src/components/TabletMobileDesign/BigBtnWithColorAll.jsx
+++ src/components/TabletMobileDesign/BigBtnWithColorAll.jsx
@@ -1,7 +1,7 @@
 import style from "../moneyGuard.module.scss"
 
 const BigBtnWithColorAll = ({valueBtn}) => {
-        return <button className={style.bigBtnWithColor} >{valueBtn}</button>
+        return <button type="submit" className={style.bigBtnWithColor} >{valueBtn}</button>
 }
 
 export default BigBtnWithColorAll
