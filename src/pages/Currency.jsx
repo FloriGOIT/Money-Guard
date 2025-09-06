7b0d25b71f2eb6187d3049e01954db093165583a
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-09-06 13:42:28 +0300

    addButton design and placement. (Ma descurc foarte bine. Am facut ce mi-am propus)

--- src/pages/Currency.jsx
+++ src/pages/Currency.jsx
@@ -1,6 +1,7 @@
 import style from '../components/moneyGuard.module.scss';
 import Nav from '../components/Nav';
 import CurrencyTable from 'components/CurrencyTable';
+import AddButton from 'components/TabletMobileDesign/AddButton';
 import { IoAddSharp } from 'react-icons/io5';
 import { Link } from 'react-router-dom';
 const Currency = () => {
@@ -12,7 +13,7 @@ const Currency = () => {
           <div className={style.currencyContainerHeader}>
             <h2>Currency exchange</h2>
             <Link to="/currency/newCoin" className={style.addCoinButton}>
-              <IoAddSharp />
+              <AddButton />
             </Link>
           </div>
 
