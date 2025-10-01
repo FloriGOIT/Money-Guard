f044f51ed82c3f69f90df49fe178a1b97660dfcd
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-10-01 19:13:41 +0300

    fetch testing

--- backend/appWithFSandPATH.js
+++ backend/appWithFSandPATH.js
@@ -4,45 +4,49 @@ const cookieParser = require("cookie-parser");
 const fs = require("fs").promises;
 const path = require("path");
 const PORT = 5000;
-const {nanoid} = require("nanoid")
+const { nanoid } = require("nanoid");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger("dev"));
 app.use(cookieParser());
 
-
-
+const APIcall =
+  "https://pixabay.com/api/?key=42799638-b50871d8c9a958480a9d6ba7c&q=london&image_type=photo&pretty=true&per_page=4";
 /*
-const isTestSuccesfull = false;
-
-const promiseTest = new Promise((resolve, reject) => {
-  if (isTestSuccesfull) {
-    resolve("Test was successfully");
-  } else {
-    reject("Test failed.");
-  }
+//new Promise(()=>{})
+const fetchData = new Promise((resolve, reject) => {
+  fetch(APIcall).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      reject("Network issues when fetching data.");
+    }
+  }).then(data => resolve(data))
+   .catch(error=>reject(error))
 });
-// varianta 1 then/catch
-//promiseTest
-//  .then((result) => console.log(result))
-//  .catch((error) => console.log(error))
-//  .finally(() => console.info("Test done!"));
+fetchData.then(data => { const info = data.hits.map(el => ({ id: el.id, type: el.type, likes: el.likes })); console.log("Selected info:", info) })
+  .catch(error => console.log("Error !!!"))
+         .finally(()=>console.log("I did it!!"))
 
-// varianta 2 try/catch
 
-const runTest = async () => {
+//try catch
+const fetchdata = async () => {
   try {
-    const result = await promiseTest;
-    console.log(result);
-  } catch (error) {
-    console.log(error);
-        }
-        finally{console.log("Test finished.")}
-};
-runTest();
+    const getData = await fetch(APIcall);
+    if (getData.ok) {
+      const prelucratedData = await getData.json();
+      const selection = prelucratedData.hits.map(el => ({ id: el.id, type: el.type, likes: el.likes })); console.log(selection)
+    }
+    else{throw Error("Error parsing data!")}
+   }
+  catch (error) {console.log("Erroring:", error)}
+  finally{console.log("Go go dolls!")}
+}
+fetchdata()
 */
 
+ 
 app.get("/", (req, res) => {
   res.send("Server is up and wrolling!");
 });
@@ -52,31 +56,35 @@ const infoLocation = path.join(__dirname, "data.json");
 app.get("/animals", async (req, res) => {
   try {
     const listAnimals = await fs.readFile(infoLocation, "utf-8");
-    console.log("Items listed successfully.")
+    console.log("Items listed successfully.");
     res.json(JSON.parse(listAnimals));
   } catch (error) {
-          console.log("Error when trying to list the animals", error);
-          res.json({message:"There has been an error when listing the information. Please recheck!"})
+    console.log("Error when trying to list the animals", error);
+    res.json({
+      message:
+        "There has been an error when listing the information. Please recheck!",
+    });
   }
 });
 
-app.post("/animals",async (req, res) => {
-        try {
-                const newItem = { id: nanoid(), ...req.body }  
-        console.log(newItem)                
-                const listAnimals = await fs.readFile(infoLocation, "utf-8");
-                console.log(listAnimals)
-                const parsedInfo = JSON.parse(listAnimals);
-                console.log("parsedInfo",parsedInfo)
-             parsedInfo.push(newItem);
-                await fs.writeFile(infoLocation, JSON.stringify(parsedInfo,null,2),{encoding:'utf-8'})
-                res.json({message: "New item was added."})
-         }
-        catch (error) {
-                console.log("Issue when appending new animal", error);
-                res.json({message:"Please recheck your request, the appending failed."})
-        }
-})
+app.post("/animals", async (req, res) => {
+  try {
+    const newItem = { id: nanoid(), ...req.body };
+    console.log(newItem);
+    const listAnimals = await fs.readFile(infoLocation, "utf-8");
+    console.log(listAnimals);
+    const parsedInfo = JSON.parse(listAnimals);
+    console.log("parsedInfo", parsedInfo);
+    parsedInfo.push(newItem);
+    await fs.writeFile(infoLocation, JSON.stringify(parsedInfo, null, 2), {
+      encoding: "utf-8",
+    });
+    res.json({ message: "New item was added." });
+  } catch (error) {
+    console.log("Issue when appending new animal", error);
+    res.json({ message: "Please recheck your request, the appending failed." });
+  }
+});
 
 app.listen(PORT, () => {
   console.log("Server is running on port 5000 -path and fs - exercise.");
