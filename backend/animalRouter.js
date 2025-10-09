7db3ec7998b8200988c55765debd81f11d624d04
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-10-09 17:03:52 +0300

    finished Mongoose exrecise

--- backend/animalRouter.js
+++ backend/animalRouter.js
@@ -1,10 +1,9 @@
 
-//express, nanoid, router, mongoose
+//express, router, mongoose
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
 const mongoose = require("mongoose");
-const { nanoid } = require("nanoid");
 const animalJoi = require("./schemas/schemaJoi.js");
 const AnimalMongo = require("./schemas/schemaMongoose.js");
 
@@ -31,33 +30,42 @@ router.post("/", async (req, res, next) => {
       await newAnimal.save();
       return res.send({ messageTry: `Animal ${newAnimal.name} was added.` })
     }
-    else{return res.send({messageTry: `Animal ${newAnimal.name} was already added.`})}
-
-   }
-  catch(error){return res.send({errorCatched: error.message})}
-})
-
-router.put("/:name", async (req, res, next) => {
-  try { 
-    const { error } = animalJoi.validate(req.body)
-    if(error){return res.send({errorJOi: error.message})}
-    const nameParam = req.params.name;
-    const dbInfo = await AnimalMongo.find({});
-    const isDuplicated = dbInfo.findIndex(el => el.name === nameParam);
-    if (isDuplicated === -1) {
-      const newAnimal = new AnimalMongo(req.body);
-      await newAnimal.save();
-      return res.send({messageTry: `Animal ${newAnimal.name} was added not updated.` })
-    }
-    else {
-      const animalFiltered = dbInfo.find(el=> el.name ===nameParam)
-      const updateAnimal = await AnimalMongo.findByIdAndUpdate(animalFiltered.id, req.body);
-      return res.send({messageTry: `Animal ${animalFiltered.name} was updated not added.`})
-
-    }
+    else { return res.send({ messageTry: `Animal ${newAnimal.name} was already added.` }) }
 
   }
-  catch(error){return res.send({errorCatched: error.message})}
+  catch (error) { return res.send({ errorCatched: error.message }) }
+});
+
+
+router.put("/:name", async (req, res, next) => {
+  try{  const { error } = animalJoi.validate(req.body);
+  if (error) { return res.send({ errorJoi: error.message }) }
+  
+  const replacedAnimal = await AnimalMongo.findOneAndReplace({ name: req.params.name }, req.body, { new: true, upsert: false })
+  if (!replacedAnimal) { return res.send({ message: `Animal does not exist in the list.` }) }
+  else{return res.send(replacedAnimal)}}
+  catch(error){return res.send({errorCaught: error.message})}
+
 })
 
+router.patch("/:name", async (req, res, next) => {
+  try {
+    const { error } = animalJoi.validate(req.body, {allowUnknown:true});
+    if (error) { return res.send({ errorJoi: error.message }) }
+    const updatePartialAnimal = await AnimalMongo.findOneAndUpdate({ name: req.params.name }, req.body, { new: true, upsert: false })
+    if (updatePartialAnimal) { return res.send({ message: updatePartialAnimal }) }
+    else{return res.send({ message: `Animal does not exist in the list.` })}
+   }
+  catch(error){return res.send({errorCaught: error.message})}
+})
+
+router.delete("/:name", async (req,res,next) => {
+  try { 
+    const deleteAnimal = await AnimalMongo.findOneAndDelete({ name: req.params.name })
+    if (!deleteAnimal) { return res.send({ messageTry: `Item not found` }) }
+    else{return res.send(deleteAnimal)}
+
+  }
+  catch(error){return res.send({errorCaught: error.message})}
+})
 module.exports=router
