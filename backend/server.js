8d403cce26c2594c44cab87ef78cd5a4a1237eb1
FloriGOIT <137000876+FloriGOIT@users.noreply.github.com>
2025-10-16 05:45:35 +0300

    ok

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express")
+require("dotenv").config();
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const creatError = require("http-errors");
+
+
+const app = express();
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+app.use((req, res, next) => {
+        next(creatError(404))
+})
+
+
+app.use((err, req, res, next) => {
+        res.status(err.status || 500)
+        res.send(`<h1>Error status: ${err.status}</h1>
+                <p>Error message: ${err.message}</p>`)
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`Server conected at ${PORT}`));
